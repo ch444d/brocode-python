6617056f42e4ba848fb7c138a2a19d9d72628cf5
Chad <71134463+ch444d@users.noreply.github.com>
2024-11-04 16:37:16 -0500

    Module 02

---
+++ 02-typecast.py
@@ -0,0 +1,20 @@
+# Typcasting - the process of converting a variable from one data type to another
+# str(), int(), float(), bool()
+
+
+name = "Bro Code"
+age = 25
+gpa = 3.7
+is_student = True
+
+# print(type(name))
+
+# gpa = int(gpa)
+# print(gpa)
+
+# age = str(age)
+# print(age)
+# print(type(age))
+
+name = bool(name)
+print(name)
