b13e2a8071bffe936987657ac70d419deefbb115
Chad <71134463+ch444d@users.noreply.github.com>
2024-11-04 16:37:02 -0500

    Module 01

---
+++ 01-variables.py
@@ -0,0 +1,54 @@
+#strings
+
+first_name = "Bro"
+last_name = "Code"
+
+print(first_name)
+print(last_name)
+print(first_name, last_name)
+
+print(f"Hello, {first_name} {last_name}")
+
+email = "@fake.com"
+
+print(f"Your email is {first_name}{email}")
+
+#integers
+
+age = 99
+quantity = 12
+num_of_students = 30
+
+
+print(f"You are {age} years old and you are buying {quantity} eggs.")
+print(f"Your class has {num_of_students} students.")
+
+#floats
+
+first_pi = 3.14
+gpa = 3.2
+
+print(f"Pi starts with {first_pi}")
+print(gpa)
+
+
+#bools
+
+is_student = True
+is_teacher = False
+
+print(is_student, is_teacher)
+
+for_sale = True
+
+if for_sale:
+    print("That item is for sale.")
+else:
+    print("That item is not for sale.")
+
+is_online = True
+
+if is_online:
+    print("You're online")
+else:
+    print("Offline")
