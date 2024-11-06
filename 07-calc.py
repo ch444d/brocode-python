945fe56c6910c34180b7bf5de8db47eb5f8aea38
Chad <71134463+ch444d@users.noreply.github.com>
2024-11-06 17:28:46 -0500

    Module 07

---
+++ 07-calc.py
@@ -0,0 +1,22 @@
+# Python Calculator
+
+operator = input("Enter an operator (+, -, *, /): ")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = 0
+
+if(operator == '+'):
+    result = num1 + num2
+    print(round(result, 3))
+elif(operator == '-'):
+    result = num1 - num2
+    print(round(result, 3))
+elif(operator == '*'):
+    result = num1 * num2
+    print(round(result, 3))
+elif(operator == '/'):
+    result = num1 / num2
+    print(round(result, 3))
+
