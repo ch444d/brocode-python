bf2f0bdf94d1ffb383e0ee148d7e12b92bbacbee
Chad <71134463+ch444d@users.noreply.github.com>
2024-11-06 12:16:32 -0500

    Module 04

---
+++ 04-madlibs.py
@@ -0,0 +1,12 @@
+
+
+adj1 = input("Enter an adjective (a description): ")
+adj2 = input("Enter an adjective (a description): ")
+adj3 = input("Enter an adjective (a description): ")
+noun1 = input("Enter a noun (person place or thing): ")
+verb1 = input("Enter a verb ending with 'ing': ")
+
+print(f"Today I went to a {adj1} zoo.")
+print(f"In an exhibit I saw a {noun1}.")
+print(f"{noun1} was {adj2} {verb1}.")
+print(f"I was {adj3}.")
