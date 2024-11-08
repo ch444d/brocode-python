84b53bd7ca37995b6890db31fcd45e31fd4100c0
Chad <71134463+ch444d@users.noreply.github.com>
2024-11-07 21:14:08 -0500

    Module 11

---
+++ 11-conditionalop.py
@@ -0,0 +1,25 @@
+# conditional expression = A one-liner shortcut for the if-else statement (ternary operator)
+# Print or assign one of two values based on a condition
+# X if condition else Y
+
+
+num = 5
+a = 6
+b = 7
+age = 1
+temperature = 25
+user_role = "Admin"
+
+# print("Positive" if num > 0 else "Negative")
+
+# result = "EVEN" if num % 2 == 0 else "ODD"
+
+# max_num = a if a > b else b
+
+# status = "Adult" if age >= 18 else "Child"
+
+# weather = "Hot" if temperature > 20 else "Cold"
+
+access_level = "Full access" if user_role == "Admin" else "Limited Access"
+
+print (access_level)
